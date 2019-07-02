e89dc6cfc2b09fe9089899de3ed71fc080ea1186
Kate Wilson <kwilson7@uw.edu>
2019-07-02 10:58:38 -0700

    need v&v version of risk observer

--- src/vivarium_conic_sqlns/components/__init__.py
+++ src/vivarium_conic_sqlns/components/__init__.py
@@ -1,4 +1,4 @@
 from .iron_deficiency import IronDeficiencyAnemia
 from .observers import DisabilityObserver, SampleHistoryObserver, RiskObserver
 from .sq_lns_intervention import SQLNSTreatmentAlgorithm, SQLNSEffect
-from .verification_and_validation import VVIronDeficiencyAnemia
+from .verification_and_validation import VVIronDeficiencyAnemia, VVRiskObserver
