bfdcdab25144a9425e8b8d04f9ab078deda50a87
James Collins <collijk@uw.edu>
2019-07-16 09:51:07 -0700

    add treatment ramp

--- src/vivarium_conic_sqlns/components/sq_lns_intervention.py
+++ src/vivarium_conic_sqlns/components/sq_lns_intervention.py
@@ -92,7 +92,7 @@ class SQLNSEffect:
                 "sd": 0.0,
                 "individual_sd": 0.0,
                 "permanent": False,
-                "ramp": 28, # Length of ramp up, ramp down time in days.
+                "ramp": 28,  # Length of ramp up, ramp down time in days.
             }
         }
     }
@@ -138,7 +138,17 @@ class SQLNSEffect:
 
     def adjust_exposure(self, index, exposure):
         effect_size = pd.Series(0, index=index)
-        treatment_groups = self._get_treatment_groups(index)
+        untreated, ramp_up, full_treatment, ramp_down, post_treatment = self._get_treatment_groups(index)
+
+        effect_size.loc[untreated] = 0
+        effect_size.loc[ramp_up] = self.ramp_efficacy(ramp_up)
+        effect_size.loc[full_treatment] = self._effect_size.loc[full_treatment]
+        if self.config.permanent:
+            effect_size.loc[ramp_down] = self._effect_size[ramp_down]
+            effect_size.loc[post_treatment] = self._effect_size[post_treatment]
+        else:
+            effect_size.loc[ramp_down] = self.ramp_efficacy(ramp_down, invert=True)
+            effect_size.loc[post_treatment] = 0
 
         return exposure + effect_size
 
@@ -147,8 +157,8 @@ class SQLNSEffect:
 
         pop = self.pop_view.get(index)
         untreated = pop.loc[(pop['sqlns_treatment_start'].isnull())
-                            | (pop['sqlns_treatment_start'] == self.clock())].index
-        ramp_up = pop.loc[(pop['sqlns_treatment_start'] > self.clock())
+                            | (pop['sqlns_treatment_start'] <= self.clock())].index
+        ramp_up = pop.loc[(pop['sqlns_treatment_start'] < self.clock())
                           & (self.clock() < pop['sqlns_treatment_start'] + ramp_time)].index
         full_treatment = pop.loc[(pop['sqlns_treatment_start'] + ramp_time <= self.clock())
                                  & (self.clock() <= pop['sqlns_treatment_end'])].index
@@ -156,9 +166,49 @@ class SQLNSEffect:
                             & (self.clock() < pop['sqlns_treatment_end'] + ramp_time)].index
         post_treatment = pop.loc[pop['sqlns_treatment_end'] + ramp_time <= self.clock()].index
 
-        return {'untreated': untreated,
-                'ramp_up': ramp_up,
-                'full_treatment': full_treatment,
-                'ramp_down': ramp_down,
-                'post_treatment': post_treatment}
+        return untreated, ramp_up, full_treatment, ramp_down, post_treatment
 
+    def ramp_efficacy(self, index, invert=False):
+        """Logistic growth/decline of effect size.
+
+        We're using a logistic function here to give a smooth treatment ramp.
+        A logistic function has the form L/(1 - e**(-k * (t - t0))
+        Where
+        L  : function maximum
+        t0 : center of the function
+        k  : growth rate
+
+        We want the function to be 0 for times below the treatment start,
+        then to ramp up to the maximum over sum duration, stay there until
+        the treatment stops, then ramp back down over the same duration.
+        This means we effectively want to squeeze a logistic function into
+        the discontinuities between a step function.  Making a function
+        that smoothly transitions would be more math than I want to do right
+        now.  Making a function that almost smoothly transitions is pretty
+        easy and involves picking a growth rate that gets us very close
+        to 0 and the maximum effect when we transition between constant
+        effect sizes and the growth periods.
+
+        I've parameterized in terms of the inverse of the  proportion of the
+        maximum effect size, p, so that the jump between the different
+        sections of the function is equal to (1 / p) * L.
+
+        """
+        if index.empty:
+            return pd.Series()
+
+        import pdb; pdb.set_trace()
+        pop = self.pop_view.get(index)
+        # 1/p is the proportion of the maximum effect.
+        # Size of the discontinuity between constant and logistic functions.
+        p = 10_000
+        growth_rate = 2 / self.config.ramp * np.log(p)
+        ramp_days = pd.Timedelta(days=self.config.ramp)
+
+        if invert:
+            ramp_position = ((pop['sqlns_treatment_end'] + ramp_days / 2) - self.clock()) / pd.Timedelta(days=1)
+        else:
+            ramp_position = (self.clock() - (pop['sqlns_treatment_start'] + ramp_days / 2)) / pd.Timedelta(days=1)
+
+        scale = 1 / (1 + np.exp(-growth_rate * ramp_position))
+        return scale * self._effect_size[index]
